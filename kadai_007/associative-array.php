5ef1fc03f537d9c0798c21ab9bf13e7f63eddd4a
donny41 <dyuandyu@gmail.com>
2024-06-07 15:35:28 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題007</title>
+</head>
+<body>
+    <?php
+        $array1 = [
+            'name' => 'onion',
+            'price' => 200,
+            'weight' => 160
+        ];
+
+        print_r($array1);
+
+    ?>
+</body>
+</html>
