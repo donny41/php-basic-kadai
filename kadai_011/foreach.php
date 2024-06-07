b621d33f96ddab71157d82c985550556b83ca732
donny41 <dyuandyu@gmail.com>
2024-06-07 16:04:02 +0900

    kadai_011提出

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題011</title>
+</head>
+<body>
+    <?php
+    $onion = [
+        '名前' => '玉ねぎ',
+        '値段' => '200',
+        '産地' => '北海等'
+    ];
+
+    foreach($onion as $k => $v){
+        echo "{$k}：{$v}<br>";
+    }
+
+    ?>
+</body>
+</html>
